98f4a15c27061f1e0a5dec99512320678e9b0e72
Knarlus <71461256+Knarlus@users.noreply.github.com>
2021-11-24 22:25:52 +0100

    genug für heute

--- Blatt06/Blatt06.c
+++ Blatt06/Blatt06.c
@@ -1,22 +1,39 @@
 #include "libBMP.h"
+#include <stdio.h>
 
-#define H 400  // heigth
-#define W 500  // width
+// width being the x coordinate
+#define HEIGTH 10
+#define WIDTH 20
 
-int main() {
-  int X,Y;
-  uint32_t* data;
-  data = (uint32_t*) malloc (W * H * sizeof(uint32_t));
+#define MIN_X - 0.5 * WIDTH
+#define MIN_Y 0
 
-  for (X = 0; X < W; X++) {
-    for (Y = 0; Y < H; Y++) {
-      if((Y/10)%2)
-        data[Y * W + X] = COLOR_RED;
-      else
-        data[Y * W + X] = COLOR_GREEN;
+int create_blank(void) {
+  int x,y;
+  uint32_t* arr;
+  arr = (uint32_t*) malloc (WIDTH * HEIGTH * sizeof(uint32_t));
+
+  for (x = 0; x < WIDTH; x++) {
+    for (y = 0; y < HEIGTH; y++) {
+      arr[y * WIDTH + x] = COLOR_WHITE;
     }
   }
-  bmp_create("plot.bmp", data, W, H);
-  free(data);
+  bmp_create("plot.bmp", arr, WIDTH, HEIGTH);
+  free(arr);
   return 0;
+}
+
+void toBMP(int matX, int matY, int *bmpX, int *bmpY) {
+  *bmpX = MIN_X + matX * (WIDTH - MIN_X) / WIDTH;
+  *bmpY = - (MIN_Y + matY * (HEIGTH - MIN_Y) / HEIGTH);
+}
+
+void test_toBMP(void) {
+  int mx, my, bmpx, bmpy;
+  for (mx = 0; mx < 20; mx+=5) {
+    for (my = 0; my < 20; my+=5) {
+      toBMP(mx, my, &bmpx, &bmpy);
+      printf("Math x: %d\tMath y: %d\tBMP x: %d\tBMP y: %d\n\n", mx, my, bmpx, bmpy);
+    }
+  }
 }
