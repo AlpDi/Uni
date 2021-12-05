bcb89086b08ba01de561588c071a64bc572b3432
Knarlus <71461256+Knarlus@users.noreply.github.com>
2021-12-05 01:52:26 +0100

    working Springerpfade

--- Blatt07/Blatt07_Arik.c
+++ Blatt07/Blatt07_Arik.c
@@ -1,38 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 // with n element of {5, 6} being the length of the sides
 #define N 5
 
-#define FIELD_START 2
-#define FIELD_EMPTY 0
-#define FIELD_MARKED 1
+int springerpfade = 0;
 
 void print_board(int board[]) {
   int i;
   for (i = 0; i < (N * N); i++) {
     printf("%d", board[i]);
-    // printf("(i / N): %d\t(N - 1): %d\t `==`: %d\n", (i / N), (N - 1), ((i / N) == (N - 1)));
     if ((i + 1) % N == 0) {
       printf("\n");
     } else {
       printf("\t");
     }
   }
-  // printf("\ni: %d\n\n", i);
 }
 
 int board_done(int board[]) {
   int i;
   for (i = 0; i < N * N; i++) {
-    if (board[i] == 1) {} else {
+    if (board[i] == 0) {
       return 0;
     }
   }
   return 1;
 }
 
-int field_exists(x, y) {
+int field_exists(int x, int y) {
   if (x < 0) {return 0;}
   if (y < 0) {return 0;}
   if (x >= N) {return 0;}
@@ -40,41 +37,39 @@ int field_exists(x, y) {
   return 1;
 }
 
-void iterate_horse_move(int current_x, int current_y, int board[]) {
-  board[current_x + current_y * N] = 1;
+void iterate_horse_move(int current_x, int current_y, int board[], int recursion_depth) {
   if (board_done(board)) {
-    printf("Board is done:\n");
+    printf("Board is done after %d recursions:\n", recursion_depth);
+    springerpfade++;
     print_board(board);
     return;
+  } //else {printf("Working in iteration %d with board:\n", recursion_depth);print_board(board);}
+  int d_x, d_y, i;
+  d_x = -3; d_y = -3; i = 1;
+  //printf("dx:dy\t%d\t%d\n", d_x, d_y);
+  while (i == 1) {
+    //printf("dx: %d\tdy: %d\t", d_x, d_y);
+    if (d_x != 0 && d_y != 0) {
+      if (abs(d_x) + abs(d_y) == 3) {
+        if (field_exists(current_x+d_x, current_y+d_y) && board[current_x + d_x + (current_y + d_y) * N] == 0) {
+          //printf("Fulfilled all parameters: dx=%d, dy=%d.\n\n", d_x, d_y);
+          board[current_x + d_x + (current_y + d_y) * N] = recursion_depth;
+          //print_board(board);printf("\n");
+          iterate_horse_move(current_x+d_x, current_y+d_y, board, recursion_depth+1);
+          board[current_x + d_x + (current_y + d_y) * N] = 0;
+        } //else {printf("NF: field (%d+%d|%d+%d) does not exist or is non empty.\n", current_x, d_x, current_y, d_y);}
+      } //else {printf("NF: wrong taximetric discance: %d + %d = %d\n", d_x, d_y, abs(d_x)+abs(d_y));}
+    } //else {printf("NF: delta on direct connection.\n");}
+    d_x = d_x + 1;
+    if (d_x > 3) {d_x = -3; d_y = d_y + 1;}
+    //printf("%d\n", d_y);
+    if (d_y > 3) {i = 0;}
   }
-  if (field_exists(current_x-1, current_y-2)) {
-    iterate_horse_move(current_x-1, current_y-2, board);
-  }
-  if (field_exists(current_x+1, current_y-2)) {
-    iterate_horse_move(current_x+1, current_y-2, board);
-  }
-  if (field_exists(current_x+2, current_y-1)) {
-    iterate_horse_move(current_x+2, current_y-1, board);
-  }
-  if (field_exists(current_x+2, current_y+1)) {
-    iterate_horse_move(current_x+2, current_y+1, board);
-  }
-  if (field_exists(current_x+1, current_y+2)) {
-    iterate_horse_move(current_x+1, current_y+2, board);
-  }
-  if (field_exists(current_x-2, current_y+2)) {
-    iterate_horse_move(current_x-2, current_y+2, board);
-  }
-  if (field_exists(current_x-2, current_y+1)) {
-    iterate_horse_move(current_x-2, current_y+1, board);
-  }
-  if (field_exists(current_x-2, current_y-1)) {
-    iterate_horse_move(current_x-2, current_y-1, board);
-  }
+  //printf("Recursion upwards!\n");
 }
 
 int main() {
-  int i, x, y, start_x, start_y; time_t t;
+  int i, start_x, start_y; time_t t;
   int* board = (int*) malloc(sizeof(int) * N * N);
 
   srand((unsigned) time(&t));
@@ -83,17 +78,14 @@ int main() {
   start_y = rand() % N;
 
   for (i = 0; i < N * N; i++) {
-    x = i % N; y = i / N;
-    if (x == start_x && y == start_y) {
-      board[i] = 2;
-    } else {
-      board[i] = 0;
-    }
-    // printf("x: %d\ty: %d\ti: %d\tboard[i]: %d\n", x, y, i, board[i]);
+    board[i] = 0;
   }
-  printf("Starting with (x|y): (%d|%d)\n\n", start_x, start_y);
+
+  board[start_x + start_y * N] = 1;
+  printf("Starting with (x|y): (%d|%d)\n", start_x, start_y);
   print_board(board);
-  iterate_horse_move(start_x, start_y, board);
+  iterate_horse_move(start_x, start_y, board, 2);
   free(board);
+  printf("\nThere were %d possible solutions found for a board of size %dx%d with starting position at (%d|%d).\n", springerpfade, N, N, start_x, start_y);
   return 0;
 }
