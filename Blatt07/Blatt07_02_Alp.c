31f92c160aa0b9ff6f8f254176c4817c06d39b6b
Alp <alpdirikgil@gmail.com>
2021-12-06 13:28:48 +0100

    .

--- Blatt07/Blatt07_02_Alp.c
+++ Blatt07/Blatt07_02_Alp.c
@@ -4,28 +4,25 @@
 #include <complex.h>
 #include <stdlib.h>
 #include "libBMP.h"
-#define W  1600
-#define H  800
+#define W  12800
+#define H  6400
 #define xMin -2.0
 #define xMax  1.0
 #define yMin -1.0
 #define yMax  1.0
-#define nMax 100
+#define nMax 200
 
 void toMath(int bmp_x , int bmp_y, double* x, double* y) {
-    if ((bmp_x < 0) || (bmp_x > W) || (bmp_y < 0) || (bmp_y > H)) {
-        *x = xMin;
-        *y = yMin;
-        return;
-    }
+    
     *x = xMin + ((double) bmp_x * (xMax - xMin)) / W;
     *y = yMin + ((double)(H - bmp_y) * (yMax - yMin)) / H;
 }
 
 int mandelbrot(double r, double iy, double zr, double ziy, int n){
-    if((n<=nMax)&&((zr*zr+ziy*ziy)<4)){
+    if((n<=nMax)&&((zr*zr+ziy*ziy)<2)){
         double zr_new = (zr*zr-ziy*ziy+r);
         double ziy_new = (2*zr*ziy+iy);
+        //punkt bleibt in menge -> schwarz
         if ((zr_new == zr) && (ziy_new == ziy)) {
             return nMax;
         }
@@ -36,7 +33,7 @@ int mandelbrot(double r, double iy, double zr, double ziy, int n){
 
 int color(double x, double y){
     int n = mandelbrot(x, y, 0.0, 0.0, 0);
-
+    //farbe repräsentiert wie weit der punkt außerhalb der menge landet
     uint32_t col = COLOR_BLACK;
     if (n < 5){ 
         col = COLOR_WHITE;
@@ -51,15 +48,15 @@ int color(double x, double y){
         col = COLOR_BLUE;
     }
 
-    //print iteration step
-    printf("%d\n", n); 
+    //Rekursionstiefe, lagt bei zu hoher auflösung und nmax code=3221225477
+    //printf("%d\n", n); 
     return col;
 }
 
 int main (){
     uint32_t* A = (uint32_t*) malloc (W * H * sizeof(uint32_t));
 
-
+    
     for(int i = 0;i < H; i++){
         for(int j = 0; j < W; j++){
             double x,y;
